1ba8e03281a406c08c264eb2e6db398b8eaf328b
ukroma1992 <ukroma1992@gmail.com>
2017-12-13 22:29:26 +0200

    Added simple auth and user model

--- Gemfile
+++ Gemfile
@@ -15,7 +15,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem "twitter-bootstrap-rails"
+gem 'simple_form'
+gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
